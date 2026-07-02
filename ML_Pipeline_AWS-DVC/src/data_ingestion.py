19f093679c71558b57c586cfd879e3f06a544abb
Subhamm__ <lastw5232@gmail.com>
2026-07-02 19:06:23 +0530

    Pipeline configured using dvc with params

--- ML_Pipeline_AWS-DVC/src/data_ingestion.py
+++ ML_Pipeline_AWS-DVC/src/data_ingestion.py
@@ -2,7 +2,7 @@ import logging
 from sklearn.model_selection import train_test_split
 import os
 import pandas as pd
-
+import yaml
 
 log_dir = 'logs'
 
@@ -28,6 +28,31 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+
+
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a yaml file"""
+    
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        
+        logger.debug('Please waitt...Yaml file is loading...')
+        return params
+    
+    except FileNotFoundError:
+        logger.error('Error!!...File not found')
+        
+    except yaml.YAMLError as e:
+        logger.error('YAML error %s', e)
+        raise
+    
+    except Exception as e:
+        logger.error('Unexpected error.. %s', e)
+        raise
+        
 
 
 def load(data_url: str) -> pd.DataFrame:
@@ -95,7 +120,10 @@ def save_data(train_data : pd.DataFrame, test_data : pd.DataFrame, data_path : s
 def main():
     
     try:
-        test_size = 0.25
+        
+        params = load_params(params_path = 'params.yaml')
+        test_size = params['data_ingestion']['test_size']
+        
         data_path = 'https://raw.githubusercontent.com/vikashishere/Datasets/refs/heads/main/spam.csv'
         df = load(data_url=data_path)
         final_df = preprocess_data(df)
--- ML_Pipeline_AWS-DVC/src/data_preprocessing.py
+++ ML_Pipeline_AWS-DVC/src/data_preprocessing.py
@@ -6,6 +6,7 @@ from nltk.stem.porter import PorterStemmer
 from nltk.corpus import stopwords
 import string
 import nltk
+import yaml
 nltk.download('stopwords')
 nltk.download('punkt')
 
@@ -30,6 +31,30 @@ file_handler.setFormatter(formatter)
 
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
+
+
+
+def load_params(params_path: str) -> dict:
+    """Load parameters from a yaml file"""
+    
+    try:
+        with open(params_path, 'r') as file:
+            params = yaml.safe_load(file)
+        
+        logger.debug('Please waitt...Yaml file is loading...')
+        return params
+    
+    except FileNotFoundError:
+        logger.error('Error!!...File not found')
+        
+    except yaml.YAMLError as e:
+        logger.error('YAML error %s', e)
+        raise
+    
+    except Exception as e:
+        logger.error('Unexpected error.. %s', e)
+        raise
+        
 
 def transform_text(text):
     """
